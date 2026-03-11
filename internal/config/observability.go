0c2b0da631498c9bb59115a259c19b896ef107be
rafidoth <rafiulhasan803@gmail.com>
2026-03-11 19:02:09 +0600

    changed service name in observability config

--- internal/config/observability.go
+++ internal/config/observability.go
@@ -35,7 +35,7 @@ type HealthChecksConfig struct {
 
 func DefaultObservabilityConfig() *ObservabilityConfig {
 	return &ObservabilityConfig{
-		ServiceName: "boilerplate",
+		ServiceName: "myapp",
 		Environment: "development",
 		Logging: LoggingConfig{
 			Level:              "info",
