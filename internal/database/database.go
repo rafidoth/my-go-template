da71c0cb75f9e009d2c4e846166dceedf676ad0b
rafidoth <rafiulhasan803@gmail.com>
2026-03-14 03:38:30 +0600

    database connection code with tracing config

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,9 +2,19 @@ package database
 
 import (
 	"context"
+	"fmt"
+	"net"
+	"net/url"
+	"strconv"
+	"time"
 
+	pgxzerolog "github.com/jackc/pgx-zerolog"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/tracelog"
+	"github.com/newrelic/go-agent/v3/integrations/nrpgx5"
+	"github.com/rafidoth/gotemp/internal/config"
+	loggerConfig "github.com/rafidoth/gotemp/internal/logger"
 	"github.com/rs/zerolog"
 )
 
@@ -28,14 +38,85 @@ func (mt *multiTracer) TraceQueryStart(ctx context.Context, conn *pgx.Conn, data
 	return ctx
 }
 
-func (mt *multiTracer) TraceQueryEnd(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryStartData) {
+func (mt *multiTracer) TraceQueryEnd(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryEndData) {
 	for _, tracer := range mt.tracers {
 		if t, ok := tracer.(interface {
-			TraceQueryEnd(context.Context, *pgx.Conn, pgx.TraceQueryStartData)
+			TraceQueryEnd(context.Context, *pgx.Conn, pgx.TraceQueryEndData)
 		}); ok {
 			t.TraceQueryEnd(ctx, conn, data)
 		}
 	}
 }
 
-const databasePingTimeout = 10
+const DatabasePingTimeout = 10
+
+func New(cfg *config.Config, logger *zerolog.Logger, loggerService *loggerConfig.LoggerService) (*Database, error) {
+	hostPort := net.JoinHostPort(cfg.Database.Host, strconv.Itoa(cfg.Database.Port))
+
+	encodedPassword := url.QueryEscape(cfg.Database.Password)
+	dsn := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s",
+		cfg.Database.User,
+		encodedPassword,
+		hostPort,
+		cfg.Database.Name,
+		cfg.Database.SSLMode,
+	)
+	fmt.Println(dsn)
+
+	pgxPoolConfig, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse pgx pool config: %w", err)
+	}
+
+	if loggerService != nil && loggerService.GetApplication() != nil {
+		pgxPoolConfig.ConnConfig.Tracer = nrpgx5.NewTracer()
+	}
+
+	if cfg.Primary.Env == "development" {
+		globalLevel := logger.GetLevel()
+		pgxLogger := loggerConfig.NewPgxLogger(globalLevel)
+
+		if pgxPoolConfig.ConnConfig.Tracer != nil {
+			localTracer := &tracelog.TraceLog{
+				Logger:   pgxzerolog.NewLogger(pgxLogger),
+				LogLevel: tracelog.LogLevel(loggerConfig.GetPgxTraceLogLevel(globalLevel)),
+			}
+			pgxPoolConfig.ConnConfig.Tracer = &multiTracer{
+				tracers: []any{pgxPoolConfig.ConnConfig.Tracer, localTracer},
+			}
+		} else {
+			pgxPoolConfig.ConnConfig.Tracer = &tracelog.TraceLog{
+				Logger:   pgxzerolog.NewLogger(pgxLogger),
+				LogLevel: tracelog.LogLevel(loggerConfig.GetPgxTraceLogLevel(globalLevel)),
+			}
+		}
+
+	}
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), pgxPoolConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create pgx pool: %w", err)
+	}
+
+	database := &Database{
+		Pool: pool,
+		log:  logger,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), DatabasePingTimeout*time.Second)
+	defer cancel()
+	if err = pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	logger.Info().Msg("connected to the database")
+
+	return database, nil
+
+}
+
+func (db *Database) Close() error {
+	db.log.Info().Msg("closing database connection pool")
+	db.Pool.Close()
+	return nil
+}
