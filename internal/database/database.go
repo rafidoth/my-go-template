10a545ea862e2630394f8089a817bcd3e71facb5
rafidoth <rafiulhasan803@gmail.com>
2026-03-14 00:47:54 +0600

    add multi tracer setup in db

---
+++ internal/database/database.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/zerolog"
+)
+
+type Database struct {
+	Pool *pgxpool.Pool
+	log  *zerolog.Logger
+}
+
+type multiTracer struct {
+	tracers []any
+}
+
+func (mt *multiTracer) TraceQueryStart(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryStartData) context.Context {
+	for _, tracer := range mt.tracers {
+		if t, ok := tracer.(interface {
+			TraceQueryStart(context.Context, *pgx.Conn, pgx.TraceQueryStartData) context.Context
+		}); ok {
+			ctx = t.TraceQueryStart(ctx, conn, data)
+		}
+	}
+	return ctx
+}
+
+func (mt *multiTracer) TraceQueryEnd(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryStartData) {
+	for _, tracer := range mt.tracers {
+		if t, ok := tracer.(interface {
+			TraceQueryEnd(context.Context, *pgx.Conn, pgx.TraceQueryStartData)
+		}); ok {
+			t.TraceQueryEnd(ctx, conn, data)
+		}
+	}
+}
+
+const databasePingTimeout = 10
